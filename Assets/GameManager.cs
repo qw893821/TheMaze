8ddb73b368cd988063abcbbc02fac2d2e6f64d87
qw893821 <xiuman893821@gmail.com>
2018-04-10 01:00:42 -0400

    update
    have tribble with turning. need more research about rotation

---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStats
+{
+    walking,
+    attack,
+    turn,
+    other
+}
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
+    public static GameManager gm
+    {
+        get { return _instance; }
+    }
+    public GameStats gs;
+	// Use this for initialization
+	void Start () {
+        if (gm == null)
+        {
+            _instance = this;
+        }
+        else if (gm != null)
+        {
+            Destroy(this);
+        }
+        gs = GameStats.other;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimeController();
+
+    }
+
+    void TimeController()
+    {
+        switch (gs){
+            case GameStats.other:
+                Time.timeScale = 0;
+                break;
+            case GameStats.attack:
+                Time.timeScale = 1;
+                break;
+            case GameStats.walking:
+                Time.timeScale = 1;
+                break;
+            default:
+                Time.timeScale = 0;
+                break;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Vector3 target;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        RotatePlayer();
+
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey("w"))
+        {
+            GameManager.gm.gs = GameStats.walking;
+        }
+        else { GameManager.gm.gs = GameStats.other; }
+        if(GameManager.gm.gs == GameStats.walking)
+        {
+            transform.position += transform.forward * Time.deltaTime;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (GameManager.gm.gs == GameStats.other) {
+            if (Input.GetKey("d"))
+            {
+                GameManager.gm.gs = GameStats.turn;
+                target = Quaternion.AngleAxis(90, Vector3.forward)*transform.position;
+            }
+            if (Input.GetKey("a"))
+            {
+                target = Quaternion.AngleAxis(-90, Vector3.forward) * transform.position;
+                GameManager.gm.gs = GameStats.turn;
+                Debug.Log(target);
+            }
+        }
+        if (GameManager.gm.gs==GameStats.turn)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, target, 0.1f,0.0f));
+        }
+    }
+}
