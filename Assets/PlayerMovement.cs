79ba39de2794ad9f3fd22790acdc3f4c49c88b55
qw893821 <xiuman893821@gmail.com>
2018-04-10 15:37:51 -0400

    update
    movement update.
    fix rotation issue

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Movement();
-        RotatePlayer();
+        //RotatePlayer();
 
     }
 
@@ -23,14 +23,40 @@ public class PlayerMovement : MonoBehaviour {
         {
             GameManager.gm.gs = GameStats.walking;
         }
+        else if (GameManager.gm.gs == GameStats.other)
+        {
+            if (Input.GetKeyDown("d"))
+            {
+                transform.Rotate(0, 90, 0);
+                GameManager.gm.gs = GameStats.turn;
+                //target = Quaternion.AngleAxis(90, Vector3.forward) * transform.position;
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                transform.Rotate(0, -90, 0);
+                //target = Quaternion.AngleAxis(-90, Vector3.forward) * transform.position;
+                GameManager.gm.gs = GameStats.turn;
+                //Debug.Log(target);
+            }
+            if (Input.GetKeyDown("s"))
+            {
+                transform.Rotate(0, 180, 0);
+                GameManager.gm.gs = GameStats.turn;
+            }
+        }
         else { GameManager.gm.gs = GameStats.other; }
+        /*if (GameManager.gm.gs == GameStats.turn)
+        {
+            transform.Rotate(0,90,0);
+            //transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, target, 0.1f, 0.0f));
+        }*/
         if(GameManager.gm.gs == GameStats.walking)
         {
             transform.position += transform.forward * Time.deltaTime;
         }
     }
 
-    void RotatePlayer()
+    /*void RotatePlayer()
     {
         if (GameManager.gm.gs == GameStats.other) {
             if (Input.GetKey("d"))
@@ -49,5 +75,5 @@ public class PlayerMovement : MonoBehaviour {
         {
             transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, target, 0.1f,0.0f));
         }
-    }
+    }*/
 }
