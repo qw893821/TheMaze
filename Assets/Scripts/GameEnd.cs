bfeac3fac41e45fc775d431668e835ada2edc735
qw893821 <xiuman893821@gmail.com>
2018-04-23 15:36:06 -0400

    update
    test scene loaded

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameEnd : MonoBehaviour {
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        text.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Player")
+        {
+            text.enabled = true;
+        }
+    }
+}
