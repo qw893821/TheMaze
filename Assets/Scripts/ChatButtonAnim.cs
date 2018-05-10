f721185d889f484b5912c1e5437efbe57b5a9247
qw893821 <xiuman893821@gmail.com>
2018-05-09 22:57:57 -0400

    update
    change to unscaled time

--- Assets/Scripts/ChatButtonAnim.cs
+++ Assets/Scripts/ChatButtonAnim.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChatButtonAnim : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    Animator anim;
+    private void OnEnable()
+    {
+        anim.SetTrigger("Enable");
+    }
+    // Use this for initialization
+    void Start () {
+        anim = transform.gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
